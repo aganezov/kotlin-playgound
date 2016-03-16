f6063488cd1e1e1ab46fcdd50c9310b7f840137f
aganezov <aganezov@gwu.edu>
2016-03-16 09:49:20 -0400

    code clean up

--- src/main/kotlin/ensembl/EnsembleProcessing.kt
+++ src/main/kotlin/ensembl/EnsembleProcessing.kt
@@ -4,21 +4,27 @@ import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
 
-val headerMapper = mapOf<String, String>(
+val ID_KEY = "id"
+val START_KEY = "start"
+val END_KEY = "end"
+val STRAND_KEY = "strand"
+val CHROMOSOME_NAME_KEY = "chr"
+
+val headerMapper = mapOf(
         // Gene id headers list
-        "Ensembl Gene ID" to "id",
+        "Ensembl Gene ID" to ID_KEY,
 
         // Gene start headers list
-        "Gene Start (bp)" to "start",
+        "Gene Start (bp)" to START_KEY,
 
         // Gene end headers list
-        "Gene End (bp)" to "end",
+        "Gene End (bp)" to END_KEY,
 
         // Gene strand headers list
-        "Strand" to "strand",
+        "Strand" to STRAND_KEY,
 
         // Chromosome name headers list
-        "Chromosome Name" to "chr"
+        "Chromosome Name" to CHROMOSOME_NAME_KEY
 )
 
 fun transformHeaders(headers: Iterable<String>) = headers.map { headerMapper.getOrElse(it, { "-1" }) }
@@ -28,9 +34,9 @@ val FILE_NAME = "chr14_genes_ensembl.txt"
 val SEPARATOR = "\t"
 
 fun main(args: Array<String>) {
-    val genes = readGeneOrder(filePath = FILE_PATH, fileName = FILE_NAME, separator = SEPARATOR)
-            .sortedBy { it.start }
+    val genes = readGeneOrder(filePath = FILE_PATH, fileName = FILE_NAME, separator = SEPARATOR).sortedBy { it.start }
     val overlappingGenes = genes.dropLast(1).zip(genes.drop(1)).filter { it.first.end > it.second.start }
+    println(overlappingGenes.size)
 
 }
 
--- src/main/kotlin/ensembl/Gene.kt
+++ src/main/kotlin/ensembl/Gene.kt
@@ -3,11 +3,11 @@ package ensembl
 fun fromCSVString(CSVString: String, headers: Iterable<String>, separator: String = "\t"): Gene {
     val data = headers.zip(CSVString.split(separator)).map { it.first to it.second }.toMap()
     return Gene(
-            id = data["id"] ?: "-1",
-            start = data["start"]?.toLong() ?: -1L,
-            end = data["end"]?.toLong() ?: -1L,
-            strand = data["strand"]?.toInt() ?: 0,
-            chromosomeName = data["chr"] ?: "-1"
+            id = data[ID_KEY] ?: "-1",
+            start = data[START_KEY]?.toLong() ?: -1L,
+            end = data[END_KEY]?.toLong() ?: -1L,
+            strand = data[STRAND_KEY]?.toInt() ?: 0,
+            chromosomeName = data[CHROMOSOME_NAME_KEY] ?: "-1"
     )
 }
 
